976412ed83683e2208f5f270ef4edb5fcfdd5d6d
kwphil <phillipsw1980@gmail.com>
2025-06-26 20:14:58 -0500

    Lots of previous commits, I know
    Fixed issue with config.lazy not existing, moved around item in init.lua
    so that if lazy fails, netrw is enabled *AND* preloaded keybinds still
    exist

--- init.lua
+++ init.lua
@@ -1,12 +1,13 @@
--- disable netrw at start
--- (For nvim-tree)
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-
-require('config.lazy')
-
 -- set 24-bit color cuz why not
 vim.opt.termguicolors = true
 
 -- And then run vimscript for keybindings and whatnot
 vim.cmd[[ source ~/.config/nvim/vim/keybinds.vim ]]
+
+-- I'm moving this here so the rest of the init setup works at least 
+require'lazysetup'
+
+-- disable netrw at start
+-- (For nvim-tree)
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
