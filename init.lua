0f0022d72e1c8fc9662f10f4bc893b7aa8b32a60
kwphil <phillipsw1980@gmail.com>
2025-06-26 20:08:26 -0500

    Cleaned up a lot of code
    init.lua had a lot of unnecessary plugin setup, and I moved
    lua/config/lazy.lua to lua/lazy.lua

--- init.lua
+++ init.lua
@@ -3,32 +3,6 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
--- Rocks setup
-local rocks_config = {
-    rocks_path = vim.env.HOME .. "/.local/share/nvim/rocks",
-}
-
-vim.g.rocks_nvim = rocks_config
-
-local luarocks_path = {
-    vim.fs.joinpath(rocks_config.rocks_path, "share", "lua", "5.1", "?.lua"),
-    vim.fs.joinpath(rocks_config.rocks_path, "share", "lua", "5.1", "?", "init.lua"),
-}
-package.path = package.path .. ";" .. table.concat(luarocks_path, ";")
-
-local luarocks_cpath = {
-    vim.fs.joinpath(rocks_config.rocks_path, "lib", "lua", "5.1", "?.so"),
-    vim.fs.joinpath(rocks_config.rocks_path, "lib64", "lua", "5.1", "?.so"),
-    -- Remove the dylib and dll paths if you do not need macos or windows support
-    vim.fs.joinpath(rocks_config.rocks_path, "lib", "lua", "5.1", "?.dylib"),
-    vim.fs.joinpath(rocks_config.rocks_path, "lib64", "lua", "5.1", "?.dylib"),
-    vim.fs.joinpath(rocks_config.rocks_path, "lib", "lua", "5.1", "?.dll"),
-    vim.fs.joinpath(rocks_config.rocks_path, "lib64", "lua", "5.1", "?.dll"),
-}
-package.cpath = package.cpath .. ";" .. table.concat(luarocks_cpath, ";")
-
-vim.opt.runtimepath:append(vim.fs.joinpath(rocks_config.rocks_path, "lib", "luarocks", "rocks-5.1", "*", "*"))
-
 require('config.lazy')
 
 -- set 24-bit color cuz why not
